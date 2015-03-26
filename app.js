d47b845b197f76c5aa14c9ac0b3a9178dbcb48c3
Matt Chandler <matt.chandler@webpt.com>
2015-03-25 21:34:17 -0700

    server setup & index route

---
+++ app.js
@@ -0,0 +1,27 @@
+// app.js
+
+/*
+ * Base Setup
+ */
+var express 	= require('express');
+var app			= express();
+var bodyParser	= require('body-parser');
+
+// config app to use bodyParser()
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+/*
+ * Routes
+ */
+var router = express.Router();
+
+router.get('/', function(req, res) {
+	res.json({
+		message:'yee'
+	});
+});
