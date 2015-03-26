6bc8f09f291a9dcf4f3b011b1c6babdea7fa0899
Matt Chandler <matt.chandler@webpt.com>
2015-03-25 22:07:15 -0700

    update user schema for username

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,7 @@ var mongoose		= require('mongoose');
 var Schema			= mongoose.Schema;
 
 var UserSchema		= new Schema({
-	name: String
+	username: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
